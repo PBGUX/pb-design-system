291af5004022913286c5b61daeef0736580e28b9
Steve Lombardi <steve.lombardi@pb.com>
2023-03-17 16:08:22 -0400

    6.18.6

---
+++ a11y-widget/a11y-widget.module.d.ts
@@ -0,0 +1,10 @@
+import * as i0 from "@angular/core";
+import * as i1 from "./a11y-widget.component";
+import * as i2 from "@angular/common";
+import * as i3 from "@angular/material/slide-toggle";
+import * as i4 from "@angular/forms";
+export declare class PbdsA11yWidgetModule {
+    static ɵfac: i0.ɵɵFactoryDeclaration<PbdsA11yWidgetModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<PbdsA11yWidgetModule, [typeof i1.PbdsA11yWidgetComponent], [typeof i2.CommonModule, typeof i3.MatSlideToggleModule, typeof i4.FormsModule, typeof i4.ReactiveFormsModule], [typeof i1.PbdsA11yWidgetComponent]>;
+    static ɵinj: i0.ɵɵInjectorDeclaration<PbdsA11yWidgetModule>;
+}
---
+++ a11y-widget/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+/// <amd-module name="pb-design-system/a11y-widget" />
+export * from './public_api';
